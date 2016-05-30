db7fa5cdd5bf4581c1f4a7fe09af0b1df10f2df8
ashwinkk123 <ashwin.karki123@yahoo.com>
2016-05-30 22:11:55 +0545

    datatypes

---
+++ MyPractise/src/com/home/util/Datatypes.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.home.util;
+
+/**
+ *
+ * @author Telus
+ */
+public class Datatypes {
+    public static void main(String args[])
+    {
+    int my_num=15; //int for 4bytes
+    float my_point=(float)10.01; //float for 4bytes
+    short my_s=10; //shot is used for 2bytes
+    double my_double=11.5; //double for 8byte 
+    char my_char='A'; //single quoation for char
+    boolean is_true=false; //boolean for true and false 
+System.out.print(my_num+ "\n");
+System.out.print(my_point+ "\n");
+System.out.print(my_s+ "\n");
+System.out.print(my_double+ "\n");
+System.out.print(my_char+ "\n");
+System.out.print(is_true);
+    }   
+}
